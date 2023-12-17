6f91a6cc6f649c2c7f84246c7ded0de26ba6ccc6
MeetWithRajat <meetwithrajat@gmail.com>
2023-12-17 14:23:12 +0530

    linked list modification

--- 20231216_01_single_linked_list.py
+++ 20231216_01_single_linked_list.py
@@ -10,6 +10,19 @@ class SingleLinkedList:
         self.__tail = None
         self.__length = 0
 
+    def __str__(self):
+        list_element = "The list is: "
+        if self.__length == 0:
+            list_element += "Empty"
+            return list_element
+        temp = self.__head
+        while temp is not None:
+            list_element += str(temp.value)
+            temp = temp.next
+            if temp is not None:
+                list_element += " -> "
+        return list_element
+
     def print_info(self):
         print(f"Head is: {type(self.__head)}\nTail is: {type(self.__tail)}\nLength is: {self.__length}")
 
@@ -17,7 +30,7 @@ class SingleLinkedList:
     def length(self):
         return self.__length
 
-    def insert_end(self, data):
+    def append(self, data):
         node = Node(data)
         if self.__head is None:
             self.__head = node
@@ -27,10 +40,14 @@ class SingleLinkedList:
             self.__tail = node
         self.__length += 1
 
-    def insert_beginning(self, data):
+    def prepend(self, data):
         node = Node(data)
-        node.next = self.__head
-        self.__head = node
+        if self.__head is None:
+            self.__head = node
+            self.__tail = node
+        else:
+            node.next = self.__head
+            self.__head = node
         self.__length += 1
 
     def insert_after_node(self, node_value, data):
@@ -43,19 +60,10 @@ class SingleLinkedList:
         else:
             node = Node(data)
             node.next = temp.next
+            if temp.next is None:
+                self.__tail = node
             temp.next = node
             self.__length += 1
-
-    def print_list(self):
-        if self.__length == 0:
-            print("Empty", end="")
-        temp = self.__head
-        while temp is not None:
-            print(temp.value, end="")
-            temp = temp.next
-            if temp is not None:
-                print(" -> ", end="")
-        print()
 
     def delete_end(self):
         if self.__length == 0:
@@ -76,26 +84,27 @@ class SingleLinkedList:
 
 my_list = SingleLinkedList()
 my_list.print_info()
-my_list.insert_end(10)
-my_list.insert_end(20)
-my_list.insert_end(30)
-my_list.insert_end(40)
-my_list.insert_beginning(0)
+print(my_list)
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.prepend(7)
+my_list.prepend(0)
+my_list.append(40)
 my_list.insert_after_node(20, 25)
 my_list.insert_after_node(0, 5)
 my_list.insert_after_node(40, 50)
-my_list.insert_after_node(56, 200)
+my_list.insert_after_node(50, 200)
+my_list.append(60)
 
-print("\nThe list is: ", end="")
-my_list.print_list()
+print(my_list)
 print()
 my_list.print_info()
 print(my_list.length)
 
-print("\nThe list is: ", end="")
-my_list.print_list()
+print(my_list)
 
 my_list.delete_end()
-print("The list is: ", end="")
-my_list.print_list()
+print(my_list)
 my_list.print_info()
+print(my_list)
